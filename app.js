66ddb007b9eec8cd70b0316c297415c23e1513d6
DannyAAM <danny@saru.moe>
2023-07-11 03:45:00 +0800

    make function more general & maybe improve some speed?

--- app.js
+++ app.js
@@ -23,23 +23,25 @@ function archiveAwait(archive, fns) {
     }
 }
 
-function appendGM9(archive, agent, url) {
+function appendZipRemap(archive, agent, url, mapping, ci) { // ci => case insensitive
+    const fns = Object.values(mapping);
+    if (ci) {
+        Object.entries(mapping).forEach(([k, v]) => {
+            mapping[ktoLowerCase()] = v;
+        });
+    }
     https.get(url, { agent }, (res) => {
         res.pipe(unzipper.Parse())
             .on('entry', (entry) => {
-                switch (entry.path.toLowerCase()) {
-                    case "GodMode9.firm".toLowerCase():
-                        archive.append(entry, { name: "luma/payloads/GodMode9.firm" });
-                        break;
-                    case "gm9/scripts/GM9Megascript.gm9".toLowerCase():
-                        archive.append(entry, { name: "finalize/GM9Megascript.gm9" });
-                        break;
-                    default:
-                        entry.autodrain();
+                const name = mapping[ci ? entry.path.toLowerCase() : entry.path];
+                if (name) {
+                    archive.append(entry, { name });
+                } else {
+                    entry.autodrain();
                 }
             });
     });
-    return archiveAwait(archive, ["luma/payloads/GodMode9.firm", "finalize/GM9Megascript.gm9"]);
+    return archiveAwait(archive, fns);
 }
 
 function appendUrl(archive, agent, fn, url) {
@@ -56,14 +58,19 @@ app.get('/finalize', async (req, res) => {
     });
     const archive = archiver('zip');
     archive.pipe(res);
-    await appendGM9(archive, agent, "https://github.com/d0k3/GodMode9/releases/download/v2.1.1/GodMode9-v2.1.1-20220322194259.zip");
-    await appendUrl(archive, agent, "gm9/scripts/finalize.gm9", "https://raw.githubusercontent.com/Gruetzig/finalizing-script/main/script/finalize.gm9");
-    await appendUrl(archive, agent, "finalize/Anemone3DS.cia", "https://github.com/astronautlevel2/Anemone3DS/releases/latest/download/Anemone3DS.cia");
-    await appendUrl(archive, agent, "finalize/Checkpoint.cia", "https://github.com/BernardoGiordano/Checkpoint/releases/download/v3.7.4/Checkpoint.cia");
-    await appendUrl(archive, agent, "finalize/Homebrew_Launcher.cia", "https://github.com/PabloMK7/homebrew_launcher_dummy/releases/latest/download/Homebrew_Launcher.cia");
-    await appendUrl(archive, agent, "finalize/FBI.cia", "https://github.com/Steveice10/FBI/releases/latest/download/FBI.cia");
-    await appendUrl(archive, agent, "finalize/ftpd.cia", "https://github.com/mtheall/ftpd/releases/latest/download/ftpd.cia");
-    await appendUrl(archive, agent, "finalize/Universal-Updater.cia", "https://github.com/Universal-Team/Universal-Updater/releases/latest/download/Universal-Updater.cia");
+    const files = [];
+    files.push(appendZipRemap(archive, agent, "https://github.com/d0k3/GodMode9/releases/download/v2.1.1/GodMode9-v2.1.1-20220322194259.zip", {
+        "GodMode9.firm": "luma/payloads/GodMode9.firm",
+        "gm9/scripts/GM9Megascript.gm9": "finalize/GM9Megascript.gm9"
+    }));
+    files.push(appendUrl(archive, agent, "gm9/scripts/finalize.gm9", "https://raw.githubusercontent.com/Gruetzig/finalizing-script/main/script/finalize.gm9"));
+    files.push(appendUrl(archive, agent, "finalize/Anemone3DS.cia", "https://github.com/astronautlevel2/Anemone3DS/releases/latest/download/Anemone3DS.cia"));
+    files.push(appendUrl(archive, agent, "finalize/Checkpoint.cia", "https://github.com/BernardoGiordano/Checkpoint/releases/download/v3.7.4/Checkpoint.cia"));
+    files.push(appendUrl(archive, agent, "finalize/Homebrew_Launcher.cia", "https://github.com/PabloMK7/homebrew_launcher_dummy/releases/latest/download/Homebrew_Launcher.cia"));
+    files.push(appendUrl(archive, agent, "finalize/FBI.cia", "https://github.com/Steveice10/FBI/releases/latest/download/FBI.cia"));
+    files.push(appendUrl(archive, agent, "finalize/ftpd.cia", "https://github.com/mtheall/ftpd/releases/latest/download/ftpd.cia"));
+    files.push(appendUrl(archive, agent, "finalize/Universal-Updater.cia", "https://github.com/Universal-Team/Universal-Updater/releases/latest/download/Universal-Updater.cia"));
+    await Promise.all(files);
     archive.finalize();
 })
 
